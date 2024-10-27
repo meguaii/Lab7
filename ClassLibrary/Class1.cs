49b2359c7dff6730919c8e3bf1a88face4d87773
uriyd <uriyd@MEGUAI>
2024-10-27 21:40:31 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plant,
+        Everything
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class CustomAttribute : Attribute
+    {
+        public string Comment { get; }
+
+        public CustomAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+
+    
+    [CustomAttribute("Это класс животного")]
+    public abstract class Animal
+    {    
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string HideFromOtherAnimals { get; set; }
+        public string WhatAnimal { get; set; }
+
+        public Animal(string name, string country, string hideFromOtherAnimals, string whatAnimal)
+        {
+            Name = name;
+            Country = country;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+            WhatAnimal = whatAnimal;
+        }
+
+
+        public virtual void GetClassificationAnimal(string whatAnimal)
+        {
+            switch (whatAnimal)
+            {
+                case "Корова":
+                    Console.WriteLine($"Классификация: {eClassificationAnimal.Herbivores}.");
+                    break;
+                case "Лев":
+                    Console.WriteLine($"Классификация: {eClassificationAnimal.Carnivores}.");
+                    break;
+                case "Свинка":
+                    Console.WriteLine($"Классификация: {eClassificationAnimal.Omnivores}.");
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная классификация.");
+                    break;
+            }
+        }
+        public virtual void Doconstruct() { }
+        public virtual void SayHello() { }
+        public virtual void GetFavouriteFood() 
+        {
+            //switch (whatAnimal)
+            //{
+            //    case "Корова":
+            //        Console.WriteLine($"любимая еда: {eFavouriteFood.Plant}.");
+            //        break;
+            //    case "Лев":
+            //        Console.WriteLine($"любимая еда: {eFavouriteFood.Meat}.");
+            //        break;
+            //    case "Свинка":
+            //        Console.WriteLine($"любимая еда: {eFavouriteFood.Everything}.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Неизвестна любимая еда.");
+            //        break;
+            //}
+        }
+
+    }
+
+    [CustomAttribute("Это класс коровы")]
+    public class Cow : Animal
+    {
+        public Cow(string name, string country, string hideFromOtherAnimals, string whatAnimal) : base(name, country, hideFromOtherAnimals, whatAnimal)
+        {
+        }
+        public override void SayHello()
+        {
+            base.SayHello();
+
+            Console.WriteLine($"Корова {Name} из страны {Country} прячется от других животных {HideFromOtherAnimals} и издает звук му-му-му.");
+        }
+
+        public override void GetFavouriteFood()
+        {
+            base.GetFavouriteFood();
+            Console.WriteLine($"любимая еда коровы это трава");
+        }
+
+    }
+
+
+    [CustomAttribute("Это класс льва")]
+    public class Lion : Animal
+    {
+        public Lion(string name, string country, string hideFromOtherAnimals, string whatAnimal) : base(name, country, hideFromOtherAnimals, whatAnimal)
+        {
+        }
+
+        public override void SayHello()
+        {
+            base.SayHello();
+            Console.WriteLine($"Лев {Name} из страны {Country} прячется от других животных {HideFromOtherAnimals} и издает звук ррр-ррр-ррр.");
+        }
+
+        public override void GetFavouriteFood()
+        {
+            base.GetFavouriteFood();
+            Console.WriteLine($"любимая еда льва это мясо");
+        }
+    }
+
+    [CustomAttribute("Это класс свиньи")]
+    public class Pig : Animal
+    {
+        public Pig(string name, string country, string hideFromOtherAnimals, string whatAnimal) : base(name, country, hideFromOtherAnimals, whatAnimal)
+        {
+        }
+
+        public override void SayHello()
+        {
+            base.SayHello();
+            Console.WriteLine($"Свинка {Name} из страны {Country} прячется от других животных {HideFromOtherAnimals} и издает звук хрю-хрю-хрю.");
+        }
+
+        public override void GetFavouriteFood()
+        {
+            base.GetFavouriteFood();
+            Console.WriteLine("любимая еда свинки это все что жуется");
+        }
+
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalReflectionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Cow("Молли", "Россия", "нет", "корова");
+            Animal lion = new Lion("Симба", "Африка", "да", "Лев");
+            Animal pig = new Pig("Пеппа", "Великобритания", "нет", "свинка");
+
+            cow.SayHello();
+            cow.GetFavouriteFood();
+
+            lion.SayHello();
+            lion.GetFavouriteFood();
+
+            pig.SayHello();
+            pig.GetFavouriteFood();
+
+            GenerateXml();
+        }
+
+        static void GenerateXml()
+        {
+            
+            var types = typeof(Animal).Assembly.GetTypes();
+            var xmlDocument = new System.Xml.Linq.XDocument(new System.Xml.Linq.XElement("Classes"));
+
+            foreach (var type in types)
+            {
+                var attribute = type.GetCustomAttribute<CustomAttribute>();
+                if (attribute != null)
+                {
+                    var classElement = new System.Xml.Linq.XElement("Class",
+                        new System.Xml.Linq.XAttribute("Name", type.Name),
+                        new System.Xml.Linq.XElement("Comment", attribute.Comment));
+                    xmlDocument.Root.Add(classElement);
+                }
+            }
+
+            string path = "AnimalClassesDiagram.xml";
+            xmlDocument.Save(path);
+            Console.WriteLine($"XML файл сгенерирован: {path}"); 
+        }
+    }
+}
